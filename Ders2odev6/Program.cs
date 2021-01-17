6fcdd8e4012518e86dc4796b76336c042d7c38a7
ilknur sercek <ilknursercek4@gmail.com>
2021-01-17 20:39:01 +0300

    16 ocak

---
+++ Ders2odev6/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Ders2odev6
+{
+    class Program
+    {
+
+    
+    public static void Main(string[] args)
+    {
+
+        Products product1 = new Products();
+        product1.ProductId = 1;
+        product1.ProductName = "Jane Eyre - Charlotte Bronte"
+  
+    Products product2 = new Products();
+        product2.ProductId = 2;
+        product2.ProductName = "Sefiller - Victor Hugo"
+  
+    Products product3 = new Products();
+        product3.ProductId = 3;
+        product3.ProductName = "Martin Eden - Jack London"
+
+
+// Ürünler diziye eklendi.
+            Products[] products = new Products()
+    {
+      product1, product2,product3
+    };
+
+        // Ürünler for döngüsünde listelendi.
+        Console.WriteLine("-- For Döngüsü --");
+        for (i = 0; i < products.Length; i++)
+        {
+            Console.WriteLine("Product Id : " + products[i].ProductId + "Product Name : " + products[i].ProductName);
+        }
+
+
+
+        // Ürünler foreach döngüsünde listelendi.
+        Console.WriteLine("-- Foreach döngüsü --");
+        foreach (var product in products)
+        {
+            Console.WriteLine("Product Id : " + product.ProductId + "Product Name : " + product.ProductName);
+        }
+
+
+    }
+}
+}
+class Products
+{
+    public string ProductName { get; set; }
+    public int ProductId { get; set; }
+
+}
---
+++ Kampintro/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Kampintro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // type safety - tip güvenliği
+            // Do not repeat yourself - Kendini Tekrarlama
+
+            string kategoriEtiketi = "Kategori";
+            int ogrenciSayisi = 32000;
+            double faizOrani = 1.45;
+            bool sistemeGirisYapmisMi = false;
+            double dolarDun = 7.55;
+            double dolarBugün = 7.45;
+
+            if (dolarDun>dolarBugün)
+            {
+                Console.WriteLine("Azalış butonu ");
+            }
+            else if (dolarDun<dolarBugün)
+            {
+                Console.WriteLine("Artış butonu");
+            }
+            else
+            {
+                Console.WriteLine("Değişmedi butonu");
+            }
+
+            if (sistemeGirisYapmisMi == true)
+            {
+                Console.WriteLine("Kullanıcı Ayarları Butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giriş yap butonu");
+            }
+            
+
+            Console.WriteLine("Kategori");
+            Console.WriteLine(kategoriEtiketi);
+
+
+        }
+    }
+}
